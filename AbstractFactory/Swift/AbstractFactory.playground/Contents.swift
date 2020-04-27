dd6fe0dbbe55174dd8b38ac3450d80ebf72c8e12
Javier Galera <javier.galera@bestsecret.com>
2020-04-27 22:46:40 +0200

    Closed #8 An example solved

---
+++ AbstractFactory/Swift/AbstractFactory.playground/Contents.swift
@@ -0,0 +1,103 @@
+enum Mode{
+    case easy
+    case hard
+}
+
+protocol ModeEnemyFactory{
+    func makeMinion()->Minion
+    func makeBoss()->Boss
+}
+
+protocol Minion{
+    func getHP()->Int
+    func getName()->String
+}
+protocol Boss{
+    func getHP()->Int
+    func getName()->String
+}
+
+class EasyEnemyFactory: ModeEnemyFactory{
+    func makeMinion()->Minion{
+        return EasyMinion()
+    }
+    func makeBoss()->Boss{
+        return EasyBoss()
+    }
+}
+
+class HardEnemyFactory: ModeEnemyFactory{
+    func makeMinion()->Minion{
+        return HardMinion()
+    }
+    func makeBoss()->Boss{
+        return HardBoss()
+    }
+}
+
+class EasyMinion: Minion{
+    let hp = 15
+    let name = "minion"
+    
+    func getHP()->Int{
+        return hp
+    }
+    func getName()->String{
+        return name
+    }
+}
+
+class EasyBoss: Boss{
+    let hp = 100
+    let name = "boss"
+    
+    func getHP()->Int{
+        return hp
+    }
+    func getName()->String{
+        return name
+    }
+}
+
+class HardMinion: Minion{
+    let hp = 50
+    let name = "super minion"
+    
+    func getHP()->Int{
+        return hp
+    }
+    func getName()->String{
+        return name
+    }
+}
+
+class HardBoss: Boss{
+    let hp = 450
+    let name = "super boss"
+    
+    func getHP()->Int{
+        return hp
+    }
+    func getName()->String{
+        return name
+    }
+}
+
+class Map {
+    var enemyFactory: ModeEnemyFactory?
+    var minion: Minion?
+    var boss: Boss?
+    
+    init(){}
+    
+    func configureMap(mode: String){
+        enemyFactory = mode == "Easy" ? EasyEnemyFactory() : HardEnemyFactory()
+        minion = enemyFactory?.makeMinion()
+        boss = enemyFactory?.makeBoss()
+        
+        print("This map has \(minion?.getName()) with \(minion?.getHP()) hp.")
+        print("You can find the final \(boss?.getName()) with \(minion?.getHP()) hp")
+    }
+}
+
+Map().configureMap(mode: "Hard")
